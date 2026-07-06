50e2e374da249988bdd91867ada87e75ba5e1f0b
Chucklee1 <kermitthefrog@kakao.com>
2026-07-06 15:22:24 -0700

    fixed auto-root-strip in fetchzips momw-tools-pack

--- momw-tools-pack/default.nix
+++ momw-tools-pack/default.nix
@@ -20,6 +20,7 @@
       aarch64-linux = fetchzip {
         url = "https://gitlab.com/modding-openmw/momw-tools-pack/-/package_files/306059120/download";
         extension = "tar.gz";
+        stripRoot = false;
         hash = "sha256-3uF+EzjpuFZqbek/A09W9CIG5ofbXDJVppwew2I7gsU=";
       };
       x86_64-linux = aarch64-linux;
@@ -27,6 +28,7 @@
       aarch64-darwin = fetchzip {
         url = "https://gitlab.com/modding-openmw/momw-tools-pack/-/package_files/306059134/download";
         extension = "tar.gz";
+        stripRoot = false;
         hash = "sha256-D24w7Ne3dsY771WaZzjlBJVS4NfQrDbUgaAuk8k+/NE=";
       };
       x86_64-darwin = aarch64-darwin;
