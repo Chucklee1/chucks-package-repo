186c03c911bad7a38154fe3fbca47e84cddae788
Chucklee1 <kermitthefrog@kakao.com>
2026-07-06 13:46:28 -0700

    added flake and packages: osu, openmw, momw-tools-pack

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A collection of a few nix packages I maintain myself";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      packages = {
+        momw-tools-pack = pkgs.callPackage ./momw-tools-pack {};
+        openmw = pkgs.callPackage ./openmw {};
+        osu = pkgs.callPackage ./osu {};
+      };
+    });
+}
